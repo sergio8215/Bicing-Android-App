54ec2e3fa0d352057fc87f1ba4bf561422846e2a
Sergio <sergio_1582@yahoo.es>
2018-12-11 14:13:49 +0100

    Map with markers and info

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bicing-Android-App
+
+An App where you can see all the availible Bicing spots in Barcelona, for each of them you can see how many bikes are availible.
+
+We use the JSON API availible on http://opendata-ajuntament.barcelona.cat/ using the JSON Simple Library and Okhttp.
---
+++ app/src/main/java/com/map/mapa/MapsActivity.java
@@ -0,0 +1,310 @@
+package com.map.mapa;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
+
+    private GoogleMap mMap;
+    private ArrayList<Vector< String>> myMarkers;
+    LocationManager locationManager;
+    double latitud, longitud;
+    LatLng latLng;
+    Marker mar;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        final TextView textView = findViewById(R.id.textView);
+
+        AsyncTask asyncTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("http://wservice.viabicing.cat/v2/stations")
+                        .build();
+                Response response = null;
+
+                try {
+
+                    response = client.newCall(request).execute();
+
+                    return response.body().string();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+
+                ArrayList<Vector< String>> markers = new ArrayList<>();
+
+                JSONObject rootJSON = null;
+                try {
+                    rootJSON = (JSONObject) new JSONParser().parse((String) o);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                JSONArray stations = (JSONArray) rootJSON.get("stations");
+                Iterator<JSONObject> iterator = stations.iterator();
+
+
+                int i = 0;
+
+                while (iterator.hasNext()) {
+
+                    JSONObject pinJSON = iterator.next();
+                    Vector<String> mark = new Vector<>();
+
+                    mark.add((String)pinJSON.get("id"));				// 0
+                    mark.add((String)pinJSON.get("type"));				// 1
+                    mark.add((String)pinJSON.get("latitude"));			// 2
+                    mark.add((String)pinJSON.get("longitude"));			// 3
+                    mark.add((String)pinJSON.get("streetName"));		// 4
+                    mark.add((String)pinJSON.get("streetNumber"));		// 5
+                    mark.add((String)pinJSON.get("altitude"));			// 6
+                    mark.add((String)pinJSON.get("slots"));				// 7
+                    mark.add((String)pinJSON.get("bikes"));				// 8
+                    mark.add((String)pinJSON.get("nearbyStations"));	// 9
+                    mark.add((String)pinJSON.get("status"));			// 10
+
+                    markers.add(mark);
+
+                    latitud = Double.parseDouble(mark.get(2));
+                    longitud = Double.parseDouble(mark.get(3));
+                    String titulo = mark.get(4);
+                    String snippet =
+                            "Puestos libres: "+
+                            mark.get(7)+" /"+
+                            "Bicicletas disponibles: "+
+                            mark.get(8)+" /"+
+                            "Estado: "+mark.get(10);
+
+                    mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(latitud,longitud))
+                            .title(titulo)
+                            .snippet(snippet));
+
+                    i++;
+                }
+                myMarkers = markers;
+
+            }
+        }.execute();
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},10);
+
+        }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        latitud = location.getLatitude();
+                        longitud = location.getLongitude();
+                        mMap.clear();
+                        latLng = new LatLng(latitud, longitud);
+                        Geocoder geocoder = new Geocoder(getApplicationContext());
+                        try {
+                            List<Address> addressList = geocoder.getFromLocation(latitud, longitud, 1);
+                            String str = addressList.get(0).getThoroughfare() + " ";
+                            str += addressList.get(0).getFeatureName() + ",";
+                            str += addressList.get(0).getLocality();
+
+                            mar = mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                });
+
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 10: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
